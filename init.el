da01766dfba33bf42524a419c5c117939b6e4a4e
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-02 09:37:30 +0100

    Add default theme

---
+++ init.el
@@ -0,0 +1,16 @@
+;;; theme-pack.el --- Theme setup
+
+;;; Commentary:
+
+;;; Code:
+
+(install-packs '(cyberpunk-theme
+                 solarized-theme))
+
+;; (disable-them 'zenburn)
+;; (load-theme 'zenburn t)
+;; (load-theme 'solarized-dark t)
+(load-theme 'cyberpunk t)
+
+
+;;; theme-pack.el ends here
