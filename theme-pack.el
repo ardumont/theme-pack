5531331ea08cf6b67f3ff7a3a78a791757793dab
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2017-05-16 16:30:30 +0200

    Increase default font-size

--- theme-pack.el
+++ theme-pack.el
@@ -55,7 +55,7 @@ ARGS With universal argument, can force the font-size to the input value."
   (let* ((hostname  (theme-pack/hostname!))
          (font-size (if font-size-input font-size-input
                       (cond ((string= hostname "dagobah") 105)
-                            ((string= hostname "corellia") 100)
+                            ((string= hostname "corellia") 110)
                             (t 80)))))
     ;; (set-frame-parameter nil 'font "DejaVu Sans Mono-16")
     (set-frame-font "-unknown-DejaVu Sans Mono-normal-normal-normal-*-16-*-*-*-m-0-iso10646-1")
